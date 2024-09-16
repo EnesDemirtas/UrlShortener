5f781f9b80078ec7cac6e54936525ddd5c4ad02f
Enes Demirtaş <enesdemirtas255@gmail.com>
2024-09-16 21:24:04 +0300

    feat: add EF Core, introduce app db context

---
+++ UrlShortener.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UrlShortener.API.Models.Domain;
+
+namespace UrlShortener.API.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Url> Urls { get; set; }
+}
